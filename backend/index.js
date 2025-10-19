d8524889eec7d9b2efcc28e3332349a77fe10fc0
Mayomibhagya <mayomibhagya.mb@gmail.com>
2025-10-19 19:31:53 +0530

    implement user authentication with signup and login routes; add User model and JWT secret to environment

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,15 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./src/config/db');
+const authRoutes = require('./src/routes/auth');
 
 const app = express();
 app.use(express.json());
 
 // connect with MongoDB
 connectDB();
+
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
   res.send('LMS Backend Running');
